f1fdafc849c2b7036660d09499067115549dad9c
huseyin <husseyn.mutl@gmail.com>
2023-10-26 15:28:13 +0300

    update ticket service

--- account-service/src/main/java/com/hsg/accountservice/configuration/CassandraConfiguration.java
+++ account-service/src/main/java/com/hsg/accountservice/configuration/CassandraConfiguration.java
@@ -1,9 +1,7 @@
 package com.hsg.accountservice.configuration;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
-import org.springframework.data.cassandra.config.SchemaAction;
 import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 
 @Configuration
